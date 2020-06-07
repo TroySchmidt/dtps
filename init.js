03a11288f09b26ee49415a5634c3ffe8d61b2169
jottocraft <11508240+jottocraft@users.noreply.github.com>
2020-06-07 15:51:59 -0700

    add init.js

---
+++ init.js
@@ -0,0 +1,8 @@
+/**
+ * @file DTPS bookmarklet loader
+ * @author jottocraft
+ * 
+ * @copyright Copyright (c) 2018-2020 jottocraft. All rights reserved.
+ * @license GPL-2.0-only
+ */
+const DTPS_ROOT_URL="https://powerplus.app/";if(window.location.hostname=="dtechhs.instructure.com"){jQuery.getScript(DTPS_ROOT_URL+"/scripts/lms/dtech.js")}else{throw"Error: Invalid domain"}
